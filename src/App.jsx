e428dad64258ba2a587a5c5f78b83345f9b2f398
Jkz6000 <106026776+Jkz2025@users.noreply.github.com>
2024-10-28 12:54:35 -0500

    Descripción de tus cambios

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/login" element={<LoginForm />} />
+          <Route path="/" element={<LoginForm />} />
           <Route 
             path="/dashboard" 
             element={
@@ -83,7 +83,7 @@ function App() {
               </PrivateRoute>
             } 
           />
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Navigate to="/dashboard" replace />} />
           <Route path="/calculadora" element={<PrivateRoute>
             <Calculadora/>
           </PrivateRoute>} />
