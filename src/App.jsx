4b7ddd6ad415e12b1230a4d5ee630e6384adc105
Jkz6000 <106026776+Jkz2025@users.noreply.github.com>
2024-10-28 13:35:31 -0500

    jkz

--- src/App.jsx
+++ src/App.jsx
@@ -74,8 +74,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />}/>
+          {/* <Route path="/" element={<Navigate to="/login" replace />}/> */}
     
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          
+          
           <Route path="/calculadora" element={<PrivateRoute>
             <Calculadora/>
           </PrivateRoute>} />
