a8bcd0e4b3f933ec82b802e06f1ae8272b5c2fac
Jkz6000 <106026776+Jkz2025@users.noreply.github.com>
2024-11-25 21:26:26 -0500

    Actualizacion Calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,4 @@
-// // ENTORNO PRUEBAS
-
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes,
-//   Navigate,
-// } from "react-router-dom";
-// import { AuthProvider } from "./constants/AuthContext";
-// import Dashboard from "./components/Dashboard";
-// import LoginForm from "./components/LoginForm";
-// import Navbar from "./components/Navbar";
-// import PrivateRoute from "./components/PrivateRoute";
-// import DashboardAsesor from "./components/DashboardAsesor/Dashboard";
-// import Inventario from "./components/Inventario";
-// import DashboardDistribuidor from "./components/DashboardDistribuidor/Dashboard";
-// import DashboardTeleventas from "./components/DashboardTeleventas/Dashboard";
-// import Calculadora from "./components/Calculadora/Calculadora";
-// import ProfileConfiguration from "./components/Configuracion/ProfileConfiguration";
-
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <Router>
-//         <Navbar />
-//         <Routes>
-//           <Route path="/login" element={<LoginForm />} />
-//           <Route path="/dashboard" element={<Dashboard />} />
-//           <Route path="/" element={<Navigate to="/dashboard" replace />} />
-//           <Route path="/catalogo" element={<Inventario />} />
-//           <Route path="/ProfileConfiguration" element={<ProfileConfiguration />} />
-//           <Route path="/calculadora" element={<Calculadora />} />
-//           <Route path="/dashboard-asesor" element={<DashboardAsesor />} />
-//           <Route
-//             path="/dashboard-distribuidor"
-//             element={<DashboardDistribuidor />}
-//           />
-//           <Route
-//             path="/dashboard-televentas"
-//             element={<DashboardTeleventas />}
-//           />
-//         </Routes>
-//       </Router>
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
-
-// ENTORNO REAL
+// ENTORNO PRUEBAS
 
 import {
   BrowserRouter as Router,
@@ -56,6 +7,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import { AuthProvider } from "./constants/AuthContext";
+import Dashboard from "./components/Dashboard";
 import LoginForm from "./components/LoginForm";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
@@ -64,7 +16,6 @@ import Inventario from "./components/Inventario";
 import DashboardDistribuidor from "./components/DashboardDistribuidor/Dashboard";
 import DashboardTeleventas from "./components/DashboardTeleventas/Dashboard";
 import Calculadora from "./components/Calculadora/Calculadora";
-import Dashboard from "./components/Dashboard";
 import ProfileConfiguration from "./components/Configuracion/ProfileConfiguration";
 
 function App() {
@@ -73,67 +24,20 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<Navigate to="/login" replace />}/> */}
-
           <Route path="/login" element={<LoginForm />} />
-          <Route
-            path="/ProfileConfiguration"
-            element={
-              <PrivateRoute>
-                <ProfileConfiguration />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/" element={<Navigate to="/login" replace />} />
-
-          <Route
-            path="/calculadora"
-            element={
-              <PrivateRoute>
-                <Calculadora />
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path="/catalogo"
-            element={
-              <PrivateRoute>
-                <Inventario />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/dashboard-asesor"
-            element={
-              <PrivateRoute>
-                <DashboardAsesor />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/catalogo" element={<Inventario />} />
+          <Route path="/ProfileConfiguration" element={<ProfileConfiguration />} />
+          <Route path="/calculadora" element={<Calculadora />} />
+          <Route path="/dashboard-asesor" element={<DashboardAsesor />} />
           <Route
             path="/dashboard-distribuidor"
-            element={
-              <PrivateRoute>
-                <DashboardDistribuidor />
-              </PrivateRoute>
-            }
+            element={<DashboardDistribuidor />}
           />
           <Route
             path="/dashboard-televentas"
-            element={
-              <PrivateRoute>
-                <DashboardTeleventas />
-              </PrivateRoute>
-            }
+            element={<DashboardTeleventas />}
           />
         </Routes>
       </Router>
@@ -142,3 +46,99 @@ function App() {
 }
 
 export default App;
+
+// ENTORNO REAL
+
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+//   Navigate,
+// } from "react-router-dom";
+// import { AuthProvider } from "./constants/AuthContext";
+// import LoginForm from "./components/LoginForm";
+// import Navbar from "./components/Navbar";
+// import PrivateRoute from "./components/PrivateRoute";
+// import DashboardAsesor from "./components/DashboardAsesor/Dashboard";
+// import Inventario from "./components/Inventario";
+// import DashboardDistribuidor from "./components/DashboardDistribuidor/Dashboard";
+// import DashboardTeleventas from "./components/DashboardTeleventas/Dashboard";
+// import Calculadora from "./components/Calculadora/Calculadora";
+// import Dashboard from "./components/Dashboard";
+// import ProfileConfiguration from "./components/Configuracion/ProfileConfiguration";
+
+// function App() {
+//   return (
+//     <AuthProvider>
+//       <Router>
+//         <Navbar />
+//         <Routes>
+//           {/* <Route path="/" element={<Navigate to="/login" replace />}/> */}
+
+//           <Route path="/login" element={<LoginForm />} />
+//           <Route
+//             path="/ProfileConfiguration"
+//             element={
+//               <PrivateRoute>
+//                 <ProfileConfiguration />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route path="/" element={<Navigate to="/login" replace />} />
+
+//           <Route
+//             path="/calculadora"
+//             element={
+//               <PrivateRoute>
+//                 <Calculadora />
+//               </PrivateRoute>
+//             }
+//           />
+
+//           <Route
+//             path="/catalogo"
+//             element={
+//               <PrivateRoute>
+//                 <Inventario />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/dashboard-asesor"
+//             element={
+//               <PrivateRoute>
+//                 <DashboardAsesor />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/dashboard"
+//             element={
+//               <PrivateRoute>
+//                 <Dashboard />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/dashboard-distribuidor"
+//             element={
+//               <PrivateRoute>
+//                 <DashboardDistribuidor />
+//               </PrivateRoute>
+//             }
+//           />
+//           <Route
+//             path="/dashboard-televentas"
+//             element={
+//               <PrivateRoute>
+//                 <DashboardTeleventas />
+//               </PrivateRoute>
+//             }
+//           />
+//         </Routes>
+//       </Router>
+//     </AuthProvider>
+//   );
+// }
+
+// export default App;
